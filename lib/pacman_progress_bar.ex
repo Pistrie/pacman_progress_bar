191fd609bf054ef8095671ef3fd0ea72590b30c3
Sylvester Roos <sylvester.roos@xs4all.nl>
2023-01-14 17:00:58 +0100

    renamed file

--- lib/pacman_progress_bar.ex
+++ lib/pacman_progress_bar.ex
@@ -2,27 +2,16 @@ defmodule PacmanProgressBar do
   @progress_character "-"
   @meal_character "o"
   @meal_spacing 2
-  @bar_size 14
+  @bar_size 30
+
+  defp percentage_completed(total_tasks, completed_tasks) do
+    (completed_tasks / total_tasks)
+    |> Float.floor(2)
+    |> Kernel.*(100)
+    |> trunc()
+  end
 
   def render do
-    initial_bar()
-  end
-
-  # initial_bar creates a progress bar where a meal is placed every x characters
-  # where x is @meal_spacing
-  defp initial_bar(bar \\ "", counter \\ 0, mealcounter \\ 0)
-
-  defp initial_bar(bar, counter, _) when counter == @bar_size do
-    bar
-  end
-
-  defp initial_bar(bar, counter, mealcounter) when mealcounter == @meal_spacing do
-    bar = bar <> @meal_character
-    initial_bar(bar, counter + 1)
-  end
-
-  defp initial_bar(bar, counter, mealcounter) when counter < @bar_size do
-    bar = bar <> @progress_character
-    initial_bar(bar, counter + 1, mealcounter + 1)
+    percentage_completed(30, 1)
   end
 end
