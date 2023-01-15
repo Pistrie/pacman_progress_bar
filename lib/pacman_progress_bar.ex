46d2f352b6ca74b4589fe909a880a964671baf6a
Sylvester Roos <sylvester.roos@xs4all.nl>
2023-01-15 12:41:44 +0100

    can now render a progress bar based on task progress

--- lib/pacman_progress_bar.ex
+++ lib/pacman_progress_bar.ex
@@ -1,4 +1,5 @@
 defmodule PacmanProgressBar do
+  @initial_pacman_character "c"
   @progress_character "-"
   @meal_character "o"
   @meal_spacing 2
@@ -8,8 +9,29 @@ defmodule PacmanProgressBar do
   # TODO create length based on width of terminal
 
   def render do
-    # "#{initial_bar()} #{percentage_completed(30, 1) |> format_percentage()}"
-    "#{initial_bar() |> add_borders_to_bar()} #{percentage_completed(30, 1) |> format_percentage()}"
+    bar = initial_bar()
+    percentage_completed = percentage_completed(30, 25)
+    destination_index = percentage_to_index(percentage_completed)
+
+    bar = move_pacman_to_index(bar, destination_index, 0, @initial_pacman_character)
+    "#{bar |> add_borders_to_bar()} #{percentage_completed |> format_percentage()}"
+  end
+
+  def render_test_progression do
+    bar = initial_bar()
+
+    Enum.each(0..30, fn tasks_completed ->
+      percentage_completed = percentage_completed(30, tasks_completed)
+      destination_index = percentage_to_index(percentage_completed)
+      bar = move_pacman_to_index(bar, destination_index, 0, @initial_pacman_character)
+
+      "\r#{bar |> add_borders_to_bar()} #{percentage_completed |> format_percentage()}"
+      |> IO.write()
+
+      :timer.sleep(500)
+    end)
+
+    IO.puts("")
   end
 
   defp add_borders_to_bar(bar) do
@@ -36,7 +58,7 @@ defmodule PacmanProgressBar do
   defp initial_bar(bar \\ "", counter \\ 0, mealcounter \\ 0)
 
   defp initial_bar(bar, counter, _) when counter == @bar_size do
-    add_borders_to_bar(bar)
+    bar |> String.to_charlist()
   end
 
   defp initial_bar(bar, counter, mealcounter) when mealcounter == @meal_spacing do
@@ -51,4 +73,63 @@ defmodule PacmanProgressBar do
 
   # TODO move pacman to the index corresponding to the percentage completed
   # make sure that the character behind pacman gets turned into @progress_character when moving him
+  defp percentage_to_index(percentage_completed) do
+    (percentage_completed / 100 * @bar_size) |> Float.floor() |> trunc()
+  end
+
+  defp move_pacman_to_index(
+         bar,
+         destination_index,
+         current_index,
+         pacman_character
+       )
+       when current_index > destination_index do
+    bar
+  end
+
+  defp move_pacman_to_index(
+         bar,
+         destination_index,
+         current_index,
+         pacman_character
+       )
+       when current_index == 0 do
+    bar = List.replace_at(bar, 0, pacman_character)
+
+    pacman_character = alternate_pacman_character(pacman_character)
+
+    move_pacman_to_index(
+      bar,
+      destination_index,
+      current_index + 1,
+      pacman_character
+    )
+  end
+
+  defp move_pacman_to_index(
+         bar,
+         destination_index,
+         current_index,
+         pacman_character
+       )
+       when current_index <= destination_index do
+    bar = List.replace_at(bar, current_index, pacman_character)
+    bar = List.replace_at(bar, current_index - 1, @progress_character)
+
+    pacman_character = alternate_pacman_character(pacman_character)
+
+    move_pacman_to_index(
+      bar,
+      destination_index,
+      current_index + 1,
+      pacman_character
+    )
+  end
+
+  defp alternate_pacman_character(pacman_character) do
+    case pacman_character do
+      "c" -> "C"
+      "C" -> "c"
+    end
+  end
 end
